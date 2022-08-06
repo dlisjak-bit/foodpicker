abf8b7f00c07473416274abff7c4c6d303288915
dlisjak-bit <domen@Domens-MacBook-Pro.local>
2022-08-06 16:51:33 +0200

    All Code Upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,22 @@
+
+
 var kovanec
 
-function CoinFlip(){
-    kovanec = 
+function MetKovanca(){
+    kovanec = Math.random();
+    console.log(kovanec);
+    if(kovanec<0.5){
+        kovanec = "glava"
+    }
+    else{
+        kovanec = "cifra"
+    }
+    console.log(kovanec)
+    document.getElementById("rezultat").innerHTML = kovanec;
+    setTimeout(() => { document.getElementById("rezultat").innerHTML = ""; }, 5000);
+
+}
+
+function IzbiraVrste(){
+
 }
