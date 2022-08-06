f2c2f8b4730f0fea141d56e88b3dae291c296880
dlisjak-bit <domen@Domens-MacBook-Pro.local>
2022-08-06 17:59:21 +0200

    All Code Upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const Kuhinje = [azijska, kitajska, indijska, ameriška, slovenska, morska, italijanska]
+const Kuhinje = ["azijska", "kitajska", "indijska", "ameriška", "slovenska", "morska", "italijanska"]
 
 function MetKovanca(){
     var kovanec = Math.random();
@@ -14,5 +14,21 @@ function MetKovanca(){
 }
 
 function IzbiraVrste(){
+    index = Math.floor(Math.random()*Kuhinje.length);
+    document.getElementById("rezultat").innerHTML = Kuhinje[index] + " hrana";
+    setTimeout(() => { document.getElementById("rezultat").innerHTML = ""; }, 10000);
+}
 
+function Restavracija(){
+    var currentUrl = window.location.href;
+    console.log(currentUrl);
+    currentUrl = currentUrl.slice(0,-10);
+    window.location = currentUrl + "pages/restavracija.html";
+}
+
+function Nazaj(){
+    var currentUrl = window.location.href;
+    console.log(currentUrl);
+    currentUrl = currentUrl.slice(0,-23);
+    window.location = currentUrl + "index.html";
 }
